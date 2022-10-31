d76abdfc9d993c6c0277ce3806bc401dc5a446b0
Supraj Maripeddi <suprajmaripeddi@C02FN3Z4Q6LR.local>
2022-10-31 14:43:51 +0530

    adding data sources

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,3 +1,5 @@
+data "aws_caller_identity" "current" {}
+
 resource "aws_s3_bucket" "sandbox_bucket1" {
   bucket = "aft-shared-${data.aws_caller_identity.current.account_id}-global-customizations"
   acl    = "private"
