30789e0a4e5b952a78640b5e192e30fe1378d205
Supraj Maripeddi <suprajmaripeddi@C02FN3Z4Q6LR.local>
2022-10-31 14:19:23 +0530

    adding new s3 bucekt as global configuration

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,4 +1,4 @@
 resource "aws_s3_bucket" "sandbox_bucket1" {
-  bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}-supraj-old"
+  bucket = "aft-shared-${data.aws_caller_identity.current.account_id}-global-customizations"
   acl    = "private"
 }
